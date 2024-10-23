bf9eda884e406188903af2e02acba07ec952cb8c
CWT-Sachin <sachin.cwt@gmail.com>
2024-10-23 09:00:11 +0530

    Version 3.1: fixed drop down issue in sizecontainer table

--- FMS-DEV/obj/Debug/net8.0/FMS-DEV.AssemblyInfo.cs
+++ FMS-DEV/obj/Debug/net8.0/FMS-DEV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FMS-DEV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae789f59dd7d2733b791b310cc4cfa0da1deeb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea1e5b065b7d21076cba043c4415558781fd0d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FMS-DEV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FMS-DEV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
